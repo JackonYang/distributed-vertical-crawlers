e99c053b05d3159db2c8b5c57f610b3c251460fd
Jackon <jiekunyang@gmail.com>
2015-08-01 11:26:10 +0800

    parse category url from food_home. 203 urls got

--- crawler/dianping.py
+++ crawler/dianping.py
@@ -2,6 +2,7 @@
 import socket
 from httplib2 import Http
 
+import re
 
 def request(url, timeout=2, method='GET'):
     """return None if timeout"""
@@ -19,6 +20,18 @@ def food_home(url='http://www.dianping.com/xian/food'):
     with open('cache/food_home.html', 'w') as f:
         f.write(content)
 
+def parse_cate():
+    with open('cache/food_home.html', 'r') as f:
+        content = ','.join(f.readlines())
+
+    cate_prog = re.compile(r'<a .*?href="(http://www.dianping.com/search/category/[\w/]+)".*?>(.*?)</a>', re.DOTALL)
+    res = cate_prog.findall(content)
+    for url, name in res:
+        print name, url
+    print len(res)
+
+
 
 if __name__ == '__main__':
-    food_home()
+    # food_home()
+    parse_cate()
