4ceb1737c495b24980d12930a31da54ab4fa13c8
Jackon <jiekunyang@gmail.com>
2015-08-01 22:53:38 +0800

    db for history or shop's reviews

--- crawler/db.py
+++ crawler/db.py
@@ -1,11 +1,15 @@
 # -*- Encoding: utf-8 -*-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import text
 from sqlalchemy import Sequence
+from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
+engine = create_engine('sqlite:///database.sqlite3')
+Base.metadata.create_all(engine)
 
 
 class download_job(Base):
@@ -20,6 +24,40 @@ class download_job(Base):
         return self.url
 
 
+class his_shop_review(Base):
+    __tablename__ = 'his_shop_review'
+
+    shop_id = Column(Integer, primary_key=True)
+    num = Column(Integer)
+    run_info = Column(String(10))
+    notes = Column(String(500))
+    timestamp = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
+
+    def __repr__(self):
+        return self.url
+
+
+def add_one(table, *args, **kwargs):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(table(*args, **kwargs))
+    session.commit()
+
+
+def add_many(obj_list):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add_all(obj_list)
+    session.commit()
+
+
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///database.sqlite3', echo=True)
-    Base.metadata.create_all(engine) 
+    add_one(his_shop_review, shop_id='1111', num=300, run_info='success')
+
+    dataset = [
+        his_shop_review(shop_id='222', num=203, run_info='success'),
+        his_shop_review(shop_id='333', num=349, run_info='success'),
+        his_shop_review(shop_id='444', num=3, run_info='success'),
+        ]
+
+    add_many(dataset)
