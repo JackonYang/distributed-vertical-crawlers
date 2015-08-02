614345f9f8e6a97fff5d4119d5389e598571382c
Jackon <jiekunyang@gmail.com>
2015-08-02 15:50:32 +0800

    parse shop star

--- crawler/parser.py
+++ crawler/parser.py
@@ -12,6 +12,20 @@ def get_files(files_dir, files_prefix=''):
     return [[f[:-5], os.path.join(files_dir, f)] for f in os.listdir(files_dir) if f.endswith('.html') and f.startswith(files_prefix)]
 
 
+def parse_shop_star(content):
+    progs = [
+        r'class="mid-rank-stars mid-str(\d+)"',
+        r'item-rank-rst irr-star(\d+)',
+        r'item-rank-rst mrr-star(\d+)',  # 21733328
+        r'mid-rank-stars mid-str(\d+)', # 2406454
+        ]
+    for p in progs:
+        m = re.compile(p, re.DOTALL).findall(content)
+        if m:
+            return m[0]
+    return ''
+
+
 def parse_shop_name(content):
     progs = [
         r'<h1 class="shop-name">\s*(.*?)\s*<.*?/h1>',
@@ -48,3 +62,5 @@ if __name__ == '__main__':
             content = ''.join(f.readlines())
 
             name = parse_shop_name(content)
+            star = parse_shop_star(content)
+            print sid, name, star
