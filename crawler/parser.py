180cff51c4e9adf466d5c180034f5911fa17927b
Jackon <jiekunyang@gmail.com>
2015-08-06 15:56:48 +0800

    refactor parser. shop name and star

--- crawler/parser.py
+++ crawler/parser.py
@@ -2,62 +2,62 @@
 import re
 import os
 
-from db import shop_profile, add_many, get_sids_db
+from log4f import debug_logger
+
+log = debug_logger('log/parser', 'parser')
+
+ignore = {
+    '15923760': u'商户不存在',
+    '21411682': u'没有评分数据',
+    }
 
 
-def find_shop_ids(content):
-    shop_prog = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"', re.DOTALL)
-    return set(shop_prog.findall(content))
+star_progs = [
+    re.compile(r'<span title="[^">]+?" class="mid-rank-stars mid-str(\d+)"></span>', re.DOTALL),
+    re.compile(r'<p class="info shop-star">\s*<span class="mid-rank-stars mid-str(\d+)"></span>', re.DOTALL),
+    re.compile(r'<div class="comment-rst">\s*<span title="[^">]+?" class="item-rank-rst [mi]rr-star(\d+)"', re.DOTALL),
+    re.compile(r'<div class="brief-info">\s*<span title="[^">]+" class="mid-rank-stars mid-str(\d+)">', re.DOTALL),
+    re.compile(r'class="mid-rank-stars mid-str(\d+) item">', re.DOTALL),
+    ]
+
+name_progs = [
+    re.compile(r'<h1 class="shop-name">\s*(.*?)\s*<.*?/h1>', re.DOTALL),
+    re.compile(r'<h1 class="shop-title" itemprop="name itemreviewed">(.*?)</h1>', re.DOTALL),
+    re.compile(r'<h2 class="market-name">(.*?)</h2>', re.DOTALL),
+    re.compile(r'<h1>(.*?)</h1>', re.DOTALL),
+    ]
 
 
 def get_files(files_dir, files_prefix=''):
     for filename in os.listdir(files_dir):
-        if filename.endswith('.html') and filename.startswith(files_prefix):
+        if filename[:-5] not in ignore and filename.endswith('.html') and filename.startswith(files_prefix):
             with open(os.path.join(files_dir, filename)) as f:
                 yield (filename[:-5], ''.join(f.readlines()))
 
 
-def parse_shop_star(content):
-    progs = [
-        r'class="mid-rank-stars mid-str(\d+)"',
-        r'item-rank-rst irr-star(\d+)',
-        r'item-rank-rst mrr-star(\d+)',  # 21733328
-        r'mid-rank-stars mid-str(\d+)', # 2406454
-        ]
-    for p in progs:
-        m = re.compile(p, re.DOTALL).findall(content)
+def parse(progs, content, id, name):
+    for idx, p in enumerate(progs):
+        m = p.findall(content)
         if m:
-            return int(m[0])
-    return 0
+            if len(m) > 1:
+                log.error('multi-match {} {}. prog-idx={}'.format(id, name, idx))
+            return m[0]
+    log.error('failed to match {} {}'.format(id, name))
+    return None
 
 
-def parse_shop_name(content):
-    progs = [
-        r'<h1 class="shop-name">\s*(.*?)\s*<.*?/h1>',
-        r'<h1 class="shop-title" itemprop="name itemreviewed">(.*?)</h1>',  # sid 17936881
-        r'<h2 class="market-name">(.*?)</h2>',  # sid 1804816
-        r'<h1>(.*?)</h1>',  # sid 19662907
-        ]
-    for p in progs:
-        m = re.compile(p, re.DOTALL).findall(content)
-        if m:
-            return m[0].decode('utf8')
-    return 'null'
+def parse_shop_star(content, sid):
+    return int(parse(star_progs, content, sid, 'star') or 0)
+
+
+def parse_shop_name(content, sid):
+    return parse(name_progs, content, sid, 'shop name') or ''
+
 
 
 if __name__ == '__main__':
-    dir_shop_profile = 'cache/shops'
-
-    data_shop_profile = []
-
-    searched = get_sids_db()
+    dir_shop_profile = 'cache/profile'
 
     for sid, content in get_files(dir_shop_profile):
-        # shop profile
-        if sid not in searched:
-            name = parse_shop_name(content)
-            star = parse_shop_star(content)
-            data_shop_profile.append(shop_profile(sid=sid, shop_name=name, star=star))
-
-    add_many(data_shop_profile)
-    print '{} new shop profile saved'.format(len(data_shop_profile))
+        name = parse_shop_name(content, sid)
+        star = parse_shop_star(content, sid)
