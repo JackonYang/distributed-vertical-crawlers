e47f79b0e22955ac20fc986682f38f8bf4361479
Jackon <jiekunyang@gmail.com>
2015-08-10 14:40:23 +0800

    init shop prof history table

--- crawler/dianping.py
+++ crawler/dianping.py
@@ -16,20 +16,33 @@ for path in [shop_prof_dir]:
     if not os.path.exists(path):
         os.makedirs(path)
 
+# const
+sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
+
 
 class ShopProfPeer(Peer):
     __tablename__ = 'ShopProfPeer'
 
 
+def init_shop_prof_job(session):
+    fn_key = lambda fn: fn.endswith('.html') and fn[:-5]
+
+    jobs = RecursiveJob(sid_ptn, ShopProfPeer, session)
+    jobs.build_idx(shop_prof_dir, fn_key)
+
+
 def grab_shop_prof(session):
     page_name = 'DianPing Shop Profile'
     url = 'http://www.dianping.com/shop/{}'
-    sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
     jobs = RecursiveJob(sid_ptn, ShopProfPeer, session)
 
     todo = jobs.get_todo()
+    if not todo:
+        init_shop_prof_job(session)
+        todo = jobs.get_todo()
+
     while todo:
-        print 'grabbing shop prof'
+        print 'grabbing shop prof. total: {}'.format(len(todo))
         builk_single(todo, url, shop_prof_dir, jobs.feed, page_name)
         todo = jobs.get_todo()
     else:
