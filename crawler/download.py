3a5c2a16f7ee5e01de2bff64581fe1158180ca03
Jackon <jiekunyang@gmail.com>
2015-08-10 10:59:14 +0800

    save job info in database

--- crawler/download.py
+++ crawler/download.py
@@ -5,14 +5,19 @@ import re
 import os
 
 from req import request, request_pages, Pagination
+from model import install
+from model import Peer
+
 from log4f import debug_logger
 
 log = debug_logger('log/download', 'download')
 
 
 class RecursiveJob:
-    def __init__(self, ptn, table=None):
+    def __init__(self, ptn, table, session):
         self.table = table  # subclass of Peer in model.py
+        self.session = session
+
         self.ptn = ptn
 
     def build_idx(self, path, parse_key):
@@ -23,10 +28,14 @@ class RecursiveJob:
             if key:
                 with open(os.path.join(path, fn)) as f:
                     self.feed(''.join(f.readlines()), key)
+        self.session.commit()
 
-    def feed(self, content, key):
+
+    def feed(self, content, key, auto_commit=True):
         data = set(self.ptn.findall(content))
-        print len(data)
+        self.session.add_all([self.table(key, i) for i in data])
+        if auto_commit:
+            self.session.commit()
         return '{} items'.format(len(data))
 
 
@@ -98,9 +107,18 @@ if __name__ == '__main__':
 
     builk_single(keys, url_pf, testdir_pf, page_name=page_name)
 
+    class ProfilePeer(Peer):
+        __tablename__ = 'ProfilePeer'
+
     sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
-    jobs = RecursiveJob(sid_ptn)
+
+    Session = install('sqlite:///test_download.sqlite3')
+    session = Session()
+
+    jobs = RecursiveJob(sid_ptn, ProfilePeer, session)
     jobs.build_idx(testdir_pf, lambda fn: fn.endswith('.html') and fn[:-5])
 
     keys2 = ['18664537', '10401458', '22124523']
     builk_single(keys2, url_pf, testdir_pf, jobs.feed, page_name)
+
+    session.close()
