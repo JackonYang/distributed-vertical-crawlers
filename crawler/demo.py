bf5249d1d032357f57391462c142bf2a5059fa44
Jackon <jiekunyang@gmail.com>
2015-08-16 10:19:56 +0800

    rename variable

--- crawler/demo.py
+++ crawler/demo.py
@@ -27,25 +27,23 @@ def init_test_path():
 def test_builk_single():
 
     init_test_path()
-    pf_path = 'test_data/dl_pf'
-    os.makedirs(pf_path)
-    pf_db = 'sqlite:///test_data/test_builk_single.sqlite3'
+    path = 'test_data/single'
+    os.makedirs(path)
+    db = 'sqlite:///test_data/single.sqlite3'
 
     keys = ['22949597', '24768319', '22124523']
-    url_pf = 'http://www.dianping.com/shop/{}'
+    url_ptn = 'http://www.dianping.com/shop/{}'
     page_name = 'dianping shop profile'
-    sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
+    item_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
 
-    Session = install(pf_db)
+    Session = install(db)
     session = Session()
 
-    jobs = RecursiveJob(sid_ptn, ProfilePeer, session)
-    # builk_single(keys, url_pf, testdir_pf, page_name=page_name)
-    jobs.build_idx(pf_path, lambda fn: fn.endswith('.html') and fn[:-5])
-
-    builk_single(keys, url_pf, pf_path, jobs.feed, page_name=page_name)
-
-    builk_single(jobs.get_todo(), url_pf, pf_path, jobs.feed, page_name)
+    jobs = RecursiveJob(item_ptn, ProfilePeer, session)
+    builk_single(keys, url_ptn, path, page_name=page_name)
+    jobs.build_idx(path, lambda fn: fn.endswith('.html') and fn[:-5])
+    builk_single(keys, url_ptn, path, jobs.feed, page_name=page_name)
+    builk_single(jobs.get_todo(), url_ptn, path, jobs.feed, page_name)
 
     session.close()
 
