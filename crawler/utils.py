946625273c00f63ee58a1f3b81b92b917cea6aee
Jackon <jiekunyang@gmail.com>
2015-08-17 23:36:41 +0800

    find todo

--- crawler/utils.py
+++ crawler/utils.py
@@ -1,5 +1,6 @@
 # -*- Encoding: utf-8 -*-
 import os
+import re
 import redis
 
 
@@ -13,20 +14,51 @@ def visited(path, conn, name, pagination=False, subfix='.html'):
     conn.sadd('{}:visited'.format(name), *data)
 
 
+def find_todo(path, conn, name, ptn, func=None):
+
+    if func is None:
+        func = lambda ptn, c, filename: set(ptn.findall(c))
+
+    visited = conn.smembers('{}:visited'.format(name))
+    print len(visited)
+
+    cache = set()
+
+    for i, fn in enumerate(os.listdir(path)):
+        if i % 5000 == 0:
+            todo = cache - visited
+            if todo:
+                conn.lpush('{}:todo'.format(name), *todo)
+            cache.clear()
+            print i, len(todo)
+        with open(os.path.join(path, fn), 'r') as f:
+            cache.update(func(ptn, ''.join(f.readlines()), filename=fn))
+
+
 if __name__ == '__main__':
 
     BASE_DIR = os.path.dirname(__file__)
     shop_review_dir = os.path.join(BASE_DIR, '../dianping/cache/shop_review')
     shop_prof_dir = os.path.join(BASE_DIR, '../dianping/cache/shop_prof')
 
-    conn = {'host': 'localhost',
+    conf = {'host': 'localhost',
             'port': 6379,
             'db': 1,
             }
 
-    r = redis.StrictRedis(**conn)
+    r = redis.StrictRedis(**conf)
     review_name = 'dp-review'
     shop_name = 'dp-shop'  # shop profile
 
     visited(shop_review_dir, r, review_name, pagination=True)
     visited(shop_prof_dir, r, shop_name, pagination=False)
+
+    rev_ptn = re.compile(r'<li[^>]+id="rev_(\d+)"')
+
+    def rev_func(ptn, c, filename):
+        if len(ptn.findall(c)) > 9:
+            return {filename[:-5]}
+
+        return set()
+
+    find_todo(shop_prof_dir, r, review_name, rev_ptn, func=rev_func)
