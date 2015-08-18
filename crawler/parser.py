0bda70b7eb3e80e3e6247bb522911413150957d1
Jackon <jiekunyang@gmail.com>
2015-08-18 12:16:16 +0800

    add Indexing

--- crawler/parser.py
+++ crawler/parser.py
@@ -1,10 +1,40 @@
 # -*- Encoding: utf-8 -*-
 import re
 import os
+import json
 
 from log4f import debug_logger
 
-log = debug_logger('log/parser', 'parser')
+log = debug_logger('log/parser', 'crawler.parser')
+
+
+class Indexing:
+    def __init__(self, ptn, filename):
+        self.ptn = ptn
+        self.filename = filename
+
+        self.data = dict()
+        if os.path.exists(filename):
+            with open(self.filename, 'r') as fr:
+                self.data = json.load(fr)
+
+    def save(self):
+        with open(self.filename, 'wb') as fw:
+            json.dump(self.data, fw, indent=4)
+
+    def scan(self, path, save_period=2000):
+        total = set(os.listdir(path))
+        todo = total - set(self.data.keys())
+        print '{}/{} to parse.'.format(len(todo), len(total))
+
+        for i, filename in enumerate(todo):
+            with open(os.path.join(path, filename)) as f:
+                c = ''.join(f.readlines())
+            self.data[filename] = self.ptn.findall(c)
+
+            if i % save_period == 0:
+                print 'saving. {} done.'.format(i+1)
+                self.save()
 
 
 def parse(progs, content, id, name, log_not_match=True, default=''):
@@ -27,3 +57,8 @@ def read_file(path, exclude, key):
     for sid, fn in files.items():
         with open(os.path.join(path, fn), 'r') as f:
             yield (sid, ''.join(f.readlines()).decode('utf8'))
+
+
+if __name__ == '__main__':
+    uid_ptn = re.compile(r'href="/member/(\d+)(?:\?[^"]+)?"')
+    Indexing(uid_ptn, '../dianping/cache/uid.json').scan('../dianping/cache/shop_prof')
