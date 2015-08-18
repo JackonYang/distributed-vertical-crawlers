5ab8d05e87de18978db5cc84165e586acb4f2348
Jackon <jiekunyang@gmail.com>
2015-08-18 22:47:45 +0800

    shop reviews with redis

--- dianping/grab.py
+++ dianping/grab.py
@@ -1,6 +1,8 @@
 # -*- Encoding: utf-8 -*-
 import re
 import os
+import redis
+import threading
 
 import sys
 parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -9,12 +11,14 @@ sys.path.append(parent)
 from argparse import ArgumentParser, FileType
 from crawler.download import RecursiveJob, builk_single, builk_pages
 from crawler.model import install, Peer, HisCount
+from crawler.parser import visited, Indexing
 
 
 # config
 BASE_DIR = os.path.dirname(__file__)
 shop_prof_dir = os.path.join(BASE_DIR, 'cache/shop_prof')
 shop_review_dir = os.path.join(BASE_DIR, 'cache/shop_review')
+shop_review_idx = os.path.join(BASE_DIR, 'cache/index/review-id.json')
 
 for path in [shop_prof_dir, shop_review_dir]:
     if not os.path.exists(path):
@@ -25,6 +29,7 @@ sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
 rev_ptn = re.compile(r'<li[^>]+id="rev_(\d+)"')
 uid_ptn = re.compile(r'href="/member/(\d+)(?:\?[^"]+)?"')
 
+rev_name = 'dp-reviews'
 find_rev = lambda c, key: set(rev_ptn.findall(c))
 
 
@@ -64,40 +69,24 @@ class ShopReviewCnt(HisCount):
     __tablename__ = 'Shop_review_cnt'
 
 
-def init_rev_idx(session):
-    fn_key = lambda fn: fn.endswith('.html') and fn[:-5]
-
-    print 'begin to build idx of files in {}'.format(path)
-    for i, fn in enumerate(os.listdir(shop_prof_dir)):
-        if i % 5000 == 0:
-            session.commit()
-            print i
-        key = fn_key(fn)
-        if key:
-            with open(os.path.join(shop_prof_dir, fn)) as f:
-                data = set(rev_ptn.findall(''.join(f.readlines())))
-                session.add(ShopReviewCnt(key, len(data)))
-
-    session.commit()
-    print 'end of build idx of files in {}'.format(path)
-
-
-def get_rev_todo(session, threshold=10):
-    reviews = session.query(ShopReviewCnt).\
-        filter(ShopReviewCnt.count > threshold-1)
-    return {i.key for i in reviews.all()}
-
-
-def grab_shop_reviews(session):
+def grab_shop_reviews(conn_redis, threshold=10):
     page_name = 'DianPing Shop Reviews'
     url = 'http://www.dianping.com/shop/{key}/review_more?pageno={page}'
 
-    todo = get_rev_todo(session)
+    idx = Indexing(rev_ptn, shop_review_idx)
+    idx.scan(shop_prof_dir)
+    total = {k[:-5] for k, cnt in idx.data.items() if len(cnt) > threshold-1}
+    done = visited(shop_review_dir, pagination=True)
+    todo = total - done
+
+    conn_redis.sadd('dp-reviews:visited', *done)
+    conn_redis.sadd('dp-reviews:todo', *todo)
+
     while todo:
         print 'grabbing shop reviews. total: {}'.format(len(todo))
         builk_pages(todo, url, shop_review_dir, find_rev,
-                    page_name=page_name, page_start=1)
-        todo = get_rev_todo(session)
+            page_name=page_name, page_start=1)
+        todo = conn_redis.smembers('dp-reviews:todo')
     else:
         print 'no shop id found'
 
@@ -106,6 +95,9 @@ if __name__ == '__main__':
     db_pf = 'sqlite:///cache/db_profile.sqlite3'
     Session = install(db_pf)
     session = Session()
+
+    r = redis.StrictRedis()
+    r.flushall()
 
     args_parser = ArgumentParser(description='Data Bang-Distributed Vertical Crawler')
     args_parser.add_argument('page', type=str,
@@ -124,8 +116,6 @@ if __name__ == '__main__':
             init_shop_prof_job(session)
         grab_shop_prof(session)
     elif page_type == 'reviews':
-        if rebuild_idx:
-            init_rev_idx(session)
-        grab_shop_reviews(session)
+        grab_shop_reviews(r)
 
     session.close()
