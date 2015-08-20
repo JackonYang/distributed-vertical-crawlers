f64bf3fb0298acc7cbd85710ff4f6b5fcf29351c
Jackon <jiekunyang@gmail.com>
2015-08-20 18:30:02 +0800

    download. job pool

--- crawler/download.py
+++ crawler/download.py
@@ -1,98 +1,118 @@
 # -*- Encoding: utf-8 -*-
 """builk requests for some kinds of web pages
+
 """
 import re
 import os
+import shutil
+import redis
 
 from req import request, request_pages
-
 from log4f import debug_logger
 
 log = debug_logger('log/download', 'download')
 
 
-class RecursiveJob:
-    def __init__(self, ptn, table, session):
-        self.table = table  # subclass of Peer in model.py
-        self.session = session
+class JobPool:
+    def __init__(self, db, job_name, timeout=10):
+        self.db = db
+        self.total_tbl = '{}:total'.format(job_name)
+        self.todo_tbl = '{}:todo'.format(job_name)
+        self.timeout = timeout
 
-        self.ptn = ptn
+    def next(self):
+        key = self.db.blpop(self.todo_tbl, self.timeout)
+        return key and key[1]
 
-    def build_idx(self, path, parse_key):
-        # full scan of a path
-        print 'begin to build idx of files in {}'.format(path)
-        for i, fn in enumerate(os.listdir(path)):
-            if i % 5000 == 0:
-                self.session.commit()
-                print i
-            key = parse_key(fn)
-            if key:
-                with open(os.path.join(path, fn)) as f:
-                    self.feed(''.join(f.readlines()), key, auto_commit=False)
-        self.session.commit()
-        print 'end of build idx of files in {}'.format(path)
-
-    def get_todo(self):
-        get = lambda col:\
-            {i[0] for i in self.session.query(col).distinct().all()}
-        return get(self.table.key2) - get(self.table.key1)
-
-    def feed(self, content, key, auto_commit=True):
-        data = set(self.ptn.findall(content))
-        self.session.add_all([self.table(key, i) for i in data])
-        if auto_commit:
-            self.session.commit()
-        return '{} items'.format(len(data))
+    def add(self, key, force=False):
+        is_new = self.db.sadd(self.total_tbl, key)
+        if force or is_new:
+            self.db.rpush(self.todo_tbl, key)
 
 
-def get_title(content, key):
+def get_title(content):
     """demo validator of builk_single"""
-    m = re.compile(r'<title>(.*?)</title>').findall(content)
-    if not m:
+    m = re.compile(r'<title>(.*?)</title>').search(content)
+    if m is None:
         return 'No Title'
-    return m[0].decode('utf8')
+    else:
+        return m.group(1)
 
 
-def builk_single(ids, url_ptn, cache_dir, validate=get_title, page_name=''):
+def builk_single(job, url_ptn, cache_dir, find_new=None):
     """builk download. one single corresponding page for one ID.
 
     usually, it is used for profile page of a user/book/shop etc.
     """
-    log_str = ''.join(['{}/', str(len(ids)),
-                       ' download ', page_name, '. ID={}'])
-    fails = set()
-    for i, key in enumerate(ids):
-        fn = os.path.join(cache_dir, '{}.html'.format(key))
-        if os.path.exists(fn):
-            log.info('{} existed. ID={}'.format(page_name, key))
-            continue
+    key = job.next()
+    print 'downloading...'
+    while key:
+        log.info('download {}'.format(key))
         url = url_ptn.format(key)
+        fn = os.path.join(cache_dir, '{}.html'.format(key))
         try:
-            log.info(log_str.format(i+1, key))
-            content = request(url, filename=fn)
-            if validate:
-                log.info(u'{} found. saved in {}'.format(
-                    validate(content, key), fn)
-                    )
+            c = request(url, filename=fn)
+            log.info('{} saved in {}'.format(get_title(c), fn))
+            if find_new:
+                for item in find_new.findall(c):
+                    job.add(item)
         except Exception as e:
-            fails.add(key)
             log.error('{}. ID={}'.format(e, key))
-    return fails
+            job.add(key, force=True)
+        key = job.next()
 
 
-def builk_pages(ids, url_ptn, cache_dir, find_items, page_name='',
+def builk_pages(job, url_ptn, cache_dir, find_item, recursive=True,
                 min_num=0, max_page=100, page_start=0):
-    log_str = ''.join(['{}/', str(len(ids)),
-                       ' download ', page_name, '. ID={}'])
     filename_ptn = os.path.join(cache_dir, '{}_{}.html')
-
-    for i, key in enumerate(ids):
+    key = job.next()
+    print 'downloading pages...'
+    while key:
+        log.info('download {}'.format(key))
         try:
-            log.info(log_str.format(i+1, key))
             ret = request_pages(key, range(page_start, max_page),
-                                url_ptn, find_items,
+                                url_ptn, find_item,
                                 min_num=min_num,
                                 filename_ptn=filename_ptn)
             log.info('{} items in {}'.format(len(ret), key))
+            if recursive:
+                for item in ret:
+                    job.add(item)
         except Exception as e:
             log.error('{}. ID={}'.format(e, key))
+            job.add(key, force=True)
+        key = job.next()
+
+
+def init_test_path():
+    path = 'test_data'
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+    return path
+
+
+if __name__ == '__main__':
+
+    init_test_path()
+    r = redis.StrictRedis()
+    r.flushall()
+
+    job = JobPool(r, 'test')
+    job.add('22124523')
+    job.add('5195730')
+
+    path = 'test_data/single'
+    os.makedirs(path)
+
+    url_pages = 'http://www.dianping.com/shop/{key}/review_more?pageno={page}'
+    rev_ptn = re.compile(r'href="/member/(\d+)">(.+?)</a>')
+    find_rev = lambda c, key: rev_ptn.findall(c)
+    builk_pages(job, url_pages, path, find_item=find_rev, recursive=False)
+
+    job.add('22124523')
+    job.add('5195730')
+
+    url_single = 'http://www.dianping.com/shop/{}'
+    sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
+    builk_single(job, url_single, path, find_new=sid_ptn)
